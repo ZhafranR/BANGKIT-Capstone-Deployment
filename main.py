969d63a7978cbd046e7ae06c9647fa02845233ff
ZhafranR <zhafranramadhan12@gmail.com>
2022-06-12 05:40:59 +0700

    forth commit

--- main.py
+++ main.py
@@ -9,5 +9,27 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 def home():
     return "Halo dunia tipu tipu!"
 
+@app.route('/station/')
+def channel_station():
+    metadata = pd.read_csv('data_temp.csv')
+    data_station = {}
+    for idx_ in range(len(metadata)):
+        each_station = {}
+        station_ = metadata.loc[idx_, 'Station']
+        station_location_ = metadata.loc[idx_, 'Station Name']
+        lat_ = metadata.loc[idx_, 'Station_Latitude']
+        long_ = metadata.loc[idx_, 'Station_Longitude']
+        elevation_ = metadata.loc[idx_, 'Elevation']
+
+        each_station.update({
+            'Station':station_,
+            'Station Location':station_location_,
+            'Latitude':lat_,
+            'Longitude':long_,
+            'Elevation':elevation_
+        })
+        data_station.update({f'Station {idx_+1}':each_station})
+    return flask.jsonify(data_station)
+
 if __name__ == "__main__":
     app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
